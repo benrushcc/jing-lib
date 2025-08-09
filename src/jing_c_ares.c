118c965a90e2e4f7ea59b49150ee8d844f6ca722
benrush <benrush0705@gmail.com>
2025-08-09 09:39:54 +0800

    skip openssl build if already exists

---
+++ src/jing_c_ares.c
@@ -0,0 +1,4 @@
+#include "jing_c_ares.h"
+#include "ares.h"
+
+int jing_ares_library_init(int flag) { return ares_library_init(flag); }
---
+++ src/jing_c_ares.h
@@ -0,0 +1,10 @@
+#ifndef JING_C_ARES_H
+#define JING_C_ARES_H
+
+#include "ares.h"
+#include "jing_common.h"
+
+
+JING_EXPORT_SYMBOL int jing_ares_library_init(int flag);
+
+#endif
